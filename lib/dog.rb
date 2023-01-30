0297e9a96f357edb0d9e10f27e1400a92da73a60
Ronald <rvpostha15@yahoo.com>
2023-01-30 17:23:13 -0500

    Tests Passing w/ Additional/Simplified Methods

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,58 @@
 class Dog
 
-end
+    def name=(dogs_name)
+      @this_dogs_name = dogs_name
+    end
+  
+    def name
+      @this_dogs_name
+    end
+  end
+  
+
+
+# class Dog
+
+#     #Setter Method
+#     def name=(value)
+#         @name = value
+#     end
+
+#     #Getter Method
+#     def name
+#     @name
+#     end
+# end
+# lassie = Dog.new
+# lassie.name = "Lassie"
+# puts lassie.name
+
+# class Dog
+#     # setter method
+#     def name=(value)
+#       @name = value
+#     end
+  
+#     # getter method
+#     def name
+#       # access the data from the @name instance variable and return it
+#       @name
+#     end
+#   end
+
+
+# class Dog
+#     attr_writer :name
+
+#     attr_reader :name
+# end
+
+# class Dog
+#     attr_accessor :name
+# end
+
+  odie = Dog.new
+  odie.name = "Odie"
+  
+  # call the Dog#name method
+  puts odie.name
